a5dee1ebba2860bb560f90e9165caa0cf8305c25
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-02-27 11:17:29 +0900

    feat: 자기소개서 페이지  구현

--- js/index.js
+++ js/index.js
@@ -1,19 +1,34 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+document.addEventListener('DOMContentLoaded', function() {
+  const submitButton = document.getElementById('sub');
+  const commentList = document.getElementById('comment-list');
+  const commentInput = document.getElementById('comment-input');
 
-console.log("알맞은 스크립트를 작성하세요");
+  submitButton.addEventListener('click', function() {
+    const commentText = commentInput.value.trim();
+
+    // 입력된 내용이 있을 경우에만 처리
+    if (commentText !== '') {
+      // 새로운 댓글을 li로 추가
+      const newComment = document.createElement('li');
+      newComment.classList.add('comment-item');
+      newComment.innerHTML = `
+        <div class="comment-author">
+          <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+          <span>방문자</span>
+        </div>
+        <div class="comment-content">
+          ${commentText}
+        </div>
+      `;
+
+      // 댓글 목록에 새 댓글 추가
+      commentList.appendChild(newComment);
+
+      // 입력 필드 초기화
+      commentInput.value = '';
+      alert("댓글이 등록되었습니다")
+    } else {
+      alert('댓글을 입력해주세요!');
+    }
+  });
+});
